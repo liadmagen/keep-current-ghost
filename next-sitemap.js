2ce20deebb72e9afe8cb12fbfec0a63c0ddb50df
liad.magen@gmail.com <liad.magen@gmail.com>
2021-03-19 17:00:35 +0100

    fix sitemap url

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 module.exports = {
   // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://next.jamify.org',
+  siteUrl: process.env.SITE_URL || 'https://keep-current.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
