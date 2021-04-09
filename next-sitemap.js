ce8b983132e821a769097cd268139514ed7ad921
liad.magen@gmail.com <liad.magen@gmail.com>
2021-04-09 14:47:57 +0200

    prevent automatic robot.txt

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 module.exports = {
   // see https://github.com/iamvishnusankar/next-sitemap
   siteUrl: process.env.SITE_URL || 'https://keep-current.vercel.app',
-  generateRobotsTxt: true,
+  // generateRobotsTxt: true,
   sitemapSize: 7000,
 }
