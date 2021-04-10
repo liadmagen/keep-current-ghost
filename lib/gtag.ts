8fd78340fb2af2955733451c11f68f77b01f394f
liad.magen@gmail.com <liad.magen@gmail.com>
2021-04-10 18:59:56 +0200

    ga - fix error when users don't concent

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,24 +1,28 @@
-export const GA_TRACKING_ID = "G-GWMYYJ9M1D";
+export const GA_TRACKING_ID = 'G-GWMYYJ9M1D'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
+  if ('gtag' in window) {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
+}
 
 type GTagEvent = {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
-};
+  action: string
+  category: string
+  label: string
+  value: number
+}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
-};
+  if ('gtag' in window) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value,
+    })
+  }
+}
