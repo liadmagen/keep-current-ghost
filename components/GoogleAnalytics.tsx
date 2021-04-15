574ce8ef0d451b405a1bf942b94ed9edbd3806e1
liad.magen@gmail.com <liad.magen@gmail.com>
2021-04-15 21:05:41 +0200

    imrove cookie concent visibility

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -30,15 +30,27 @@ export const GoogleAnalytics = () => {
       </Head>
 
       <CookieConsent
-        location="bottom"
+        overlay={true}
+        overlayStyle={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: "9999",
+          backgroundColor: "rgba(0,0,0,0.3)",
+        }}
+        location="top"
         enableDeclineButton={true}
-        buttonText="I agree"
-        style={{ background: '#333333' }}
+        setDeclineCookie={false}
+        buttonText="Opt in"
+        declineButtonText="Sorry, no"
+        style={{ background: '#333333', zIndex: 9999}}
         buttonStyle={{ backgroundColor: '#99AA38', color: '#eee', fontSize: '16px', fontWeight: 'bold' }}
         onAccept={() => {setConcent('true');}}
       >
         We are using 3rd party cookies to track content popularity, as well as storing your concent to this annoying message, so you won't see it again... By agreeing, you permit these cookies.{' '}
-        <a href="/privacy">Learn more</a>
+        <a href="/privacy">Read our <i>Privacy Policy</i></a>
       </CookieConsent>
     </>
   )
