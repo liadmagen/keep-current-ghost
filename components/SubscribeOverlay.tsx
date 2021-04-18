0957c4ca581c55060a560e7238c8d227367b3c0c
liad.magen@gmail.com <liad.magen@gmail.com>
2021-04-18 14:10:48 +0200

    use invert logo on subscription screen

--- components/SubscribeOverlay.tsx
+++ components/SubscribeOverlay.tsx
@@ -4,7 +4,7 @@ import { CSSProperties } from 'react'
 import { useOverlay } from '@components/contexts/overlayProvider'
 import { GhostSettings } from '@lib/ghost'
 import { useLang, get } from '@utils/use-lang'
-import { siteIcon } from '@meta/siteDefaults'
+// import { siteIcon } from '@meta/siteDefaults'
 
 import { SubscribeForm } from '@components/SubscribeForm'
 
@@ -13,7 +13,7 @@ export const SubscribeOverlay = ({ settings }: { settings: GhostSettings }) => {
   const { isOpen, handleClose } = useOverlay()
 
   const title = text(`SITE_TITLE`, settings.title)
-  const siteLogo = settings.logo || siteIcon
+  // const siteLogo = settings.logo || siteIcon
   const openingStyle: CSSProperties = { opacity: 1, pointerEvents: `auto` }
   const closingStyle: CSSProperties = { opacity: 0, pointerEvents: `none` }
 
@@ -22,9 +22,7 @@ export const SubscribeOverlay = ({ settings }: { settings: GhostSettings }) => {
       <a className="subscribe-close-overlay" onClick={handleClose}></a>
       <a className="subscribe-close-button" onClick={handleClose}></a>
       <div className="subscribe-overlay-content">
-        {siteLogo &&
-          <img className="subscribe-overlay-logo" src={siteLogo} alt={title} />
-        }
+        <img className="subscribe-overlay-logo" src="/images/keep_current_invert_transparent_small.png" alt={title} />
         <div className="subscribe-form">
           <h1 className="subscribe-overlay-title">{text(`SUBSCRIBE_TO`)} {title}</h1>
           <p className="subscribe-overlay-description">{text(`SUBSCRIBE_OVERLAY`)}</p>
